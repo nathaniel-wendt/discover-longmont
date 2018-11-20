1d5bbe00d9c7da81788ad44c1e942e237a9444a9
Nathaniel <nathaniel.wendt@gmail.com>
2018-11-19 23:54:24 -0700

    feat: add Google MapsAPI connectivity

--- src/App.js
+++ src/App.js
@@ -8,7 +8,24 @@ class App extends Component {
   state = {
     locations: []
   }
-  
+
+  componentDidMount() {
+    this.renderMap()
+  }
+
+  renderMap = () => {
+    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyAp2UAcZ8h6HlJ_J3Ha9qZcbt6ohSR9yFs&callback=initMap")
+    window.initMap = this.initMap
+  }
+
+  // Initialize Map by setting Map Center and adding Map Markers using Lat Lng
+  initMap = () => {
+    let map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: 40.1672, lng: -105.1019},
+      zoom: 13
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -17,6 +34,16 @@ class App extends Component {
       </div>
     );
   }
+}
+
+/* learned from Yahya Elharony's YouTube Video: http://www.youtube.com/watch?v=W5LhLZqj76s */
+function loadScript(url) {
+  let index = window.document.getElementsByTagName("script")[0]
+  let script = window.document.createElement('script')
+  script.src = url
+  script.async = true
+  script.defer = true
+  index.parentNode.insertBefore(script, index)
 }
 
 export default App;
