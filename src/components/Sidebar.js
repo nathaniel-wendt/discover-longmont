0d78a727894d073545d8a2b0c2bd9fb740393261
Nathaniel <nathaniel.wendt@gmail.com>
2018-11-19 23:13:28 -0700

    feat: add collapsible sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 
 class Sidebar extends Component {
+
+    toggleSidebar() {
+        document.getElementById('sidebar').classList.toggle('active');
+    }
+
   render() {
     return (
         <div id="sidebar">
-
+            <input type={"search"} id={"search"} placeholder={"Search Locations"} />
+            <div class="toggle-btn" onClick={this.toggleSidebar}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <ul>
+                <li>1st Result</li>
+                <li>2nd Result</li>
+                <li>3rd Result</li>
+            </ul>
         </div>
     );
   }
