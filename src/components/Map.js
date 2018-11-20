a87d744bf6da5db9f312bb857ed38892476ebc2d
Nathaniel <nathaniel.wendt@gmail.com>
2018-11-19 21:54:48 -0700

    initial commit: Add blank Map and Sidebar Components

---
+++ src/components/Map.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Map extends Component {
+  render() {
+    return (
+        <div id="map" role="application" aria-label="Map" />
+    );
+  }
+}
+
+export default Map;
